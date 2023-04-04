0e35698523746794602236927076bdad700acb8d
sophiamoos1 <90751245+sophiamoos1@users.noreply.github.com>
2023-04-04 14:30:38 +0200

    styling

--- src/components/atoms/LoadingProgressNormal.tsx
+++ src/components/atoms/LoadingProgressNormal.tsx
@@ -1,57 +1,85 @@
-import React from 'react';
-import {Box, CircularProgress, Fade, Grid} from "@mui/material";
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import React from "react";
+import { Box, CircularProgress, Fade, Grid } from "@mui/material";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import Typography from "@mui/material/Typography";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 
 type LoadingProgressType = {
-    text: string;
-    sx: React.CSSProperties;
-    delayTime: number;
-    setSuccess: Function;
-}
+  text: string;
+  sx: React.CSSProperties;
+  delayTime: number;
+  setSuccess: Function;
+};
 
-export default function LoadingProgressNormal({text,sx, delayTime, setSuccess}: LoadingProgressType) {
-    const [query, setQuery] = React.useState("idle");
-    const timerRef = React.useRef<number>();
-    React.useEffect(()=> {
-        if (timerRef.current) {
-            clearTimeout(timerRef.current);
-        }
+export default function LoadingProgressNormal({
+  text,
+  sx,
+  delayTime,
+  setSuccess,
+}: LoadingProgressType) {
+  const [query, setQuery] = React.useState("idle");
+  const timerRef = React.useRef<number>();
+  React.useEffect(() => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
 
-        if (query !== "idle") {
-            setQuery("idle");
-            return;
-        }
+    if (query !== "idle") {
+      setQuery("idle");
+      return;
+    }
 
-        setQuery("progress");
-        timerRef.current = window.setTimeout(() => {
-            setQuery("success");
-            setSuccess(true);
-        }, delayTime);
-    },[])
+    setQuery("progress");
+    timerRef.current = window.setTimeout(() => {
+      setQuery("success");
+      setSuccess(true);
+    }, delayTime);
+  }, []);
 
-    return (
-        <Box>
-            <Grid container sx={{pt: 2}}>
-                <Grid item xs={6} sx={{textAlign: "left"}}>
-                    <Typography variant="body1" sx={{fontSize: "16px", color: "#fff"}}>{text}</Typography>
-                </Grid>
-                <Grid item xs={6}>
-                    {query === "success" ? (
-                        <CheckBoxIcon sx={{color: "#a2ff7d"}}/>
-                    ) : (
-                        <Fade
-                            in={query === "progress"}
-                            style={{
-                                transitionDelay: query === "progress" ? "800ms" : "0ms"
-                            }}
-                            unmountOnExit
-                        >
-                            <CircularProgress style={sx}/>
-                        </Fade>
-                    )}
-                </Grid>
-            </Grid>
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        bgcolor: "rgba(56,56,56,0.4)",
+        height: "4vh",
+        mb: "1vh",
+        justifyContent: "center",
+        width: "90%",
+      }}
+    >
+      <Grid container>
+        <Grid item xs={0.5} sx={{ textAlign: "left" }}>
+          <Box
+            sx={{ bgcolor: "#fcca27", height: "4vh", pr: 2, width: "2px" }}
+          />
+        </Grid>
+        <Grid item xs={2.5}>
+          {query === "success" ? (
+            <CheckBoxIcon sx={{ color: "#a2ff7d" }} />
+          ) : (
+            <CheckBoxOutlineBlankIcon sx={{ color: "#a2ff7d" }} />
+          )}
+        </Grid>
+        <Grid item xs={5} sx={{ textAlign: "left", justifyContent: "center" }}>
+          <Typography variant="body1" sx={{ fontSize: "16px", color: "#fff" }}>
+            {text}
+          </Typography>
+        </Grid>
+        <Grid item xs={4}>
+          {query === "success" ? (
+            <></>
+          ) : (
+            <Fade
+              in={query === "progress"}
+              style={{
+                transitionDelay: query === "progress" ? "800ms" : "0ms",
+              }}
+              unmountOnExit
+            >
+              <CircularProgress style={sx} size={30} />
+            </Fade>
+          )}
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
