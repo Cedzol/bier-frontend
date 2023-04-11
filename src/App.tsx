b146ef13b417d40d077df523b1c58603e45cfd36
sophiamoos1 <90751245+sophiamoos1@users.noreply.github.com>
2023-04-11 13:11:25 +0200

    added styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import "./App.css";
-import MainPage from "./components/pages/MainPage";
 import { Brewmaster } from "./components/Brewmaster";
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
-
       <Brewmaster />
     </div>
   );
--- src/components/Brewmaster.tsx
+++ src/components/Brewmaster.tsx
@@ -1,34 +1,47 @@
-import React, {useState} from 'react'
-import {BeerType} from "../assets/BeerType";
+import React, { useState } from "react";
+import { BeerType } from "../assets/BeerType";
+import MainPage from "./pages/MainPage";
+import { Box } from "@mui/material";
 
-export function Brewmaster(){
-    const [beerType, setBeerType] : any= useState(BeerType.Lager);
-    const [showBeerSelection, setShowBeerSelection] = useState(true);
+export function Brewmaster() {
+  const [beerType, setBeerType]: any = useState(BeerType.Lager);
+  const [showBeerSelection, setShowBeerSelection] = useState(true);
 
-    function changeBeerType(){
-        let select : any = document.getElementById("beerTypeSelect");
-        console.log("beertype changed")
-        setBeerType(select.value)
-    }
+  function changeBeerType() {
+    let select: any = document.getElementById("beerTypeSelect");
+    console.log("beertype changed");
+    setBeerType(select.value);
+  }
 
-    function startProccess(){
-        setShowBeerSelection(false);
-    }
+  function startProccess() {
+    setShowBeerSelection(false);
+  }
 
-
-    return (
+  return (
+    <div>
+      <p>Selected Beer Type: {beerType}</p>
+      {showBeerSelection ? (
         <div>
-            <p>Selected Beer Type: {beerType}</p>
-            {showBeerSelection ? <div>
-                <select id={"beerTypeSelect"} onChange={changeBeerType}>
-                <option value={BeerType.Dunkel}>Dunkel</option>
-                <option value={BeerType.Lager}>Lager</option>
-                <option value={BeerType.Weizen}>Weizen</option>
-                </select>
-                    <br></br>
-                <button onClick={startProccess}>Start Prozesss</button>
-                </div>
-                : <div></div>}
+          <select id={"beerTypeSelect"} onChange={changeBeerType}>
+            <option value={BeerType.Dunkel}>Dunkel</option>
+            <option value={BeerType.Lager}>Lager</option>
+            <option value={BeerType.Weizen}>Weizen</option>
+          </select>
+          <br></br>
+          <button onClick={startProccess}>Start Prozesss</button>
         </div>
-    )
+      ) : (
+        <Box
+          sx={{
+            height: "100vh",
+            width: "100vw",
+            bgcolor: "#191919",
+            overflow: "hidden",
+          }}
+        >
+          <MainPage />
+        </Box>
+      )}
+    </div>
+  );
 }
